2f1300ac6ee5e8c231ff6dcf2cc306ac6a291e8b
vhrozovsky <vhrozovsky@LinuxUbuntu.myguest.virtualbox.org>
2024-03-17 21:49:45 +0200

    Mypy: simple file to test

---
+++ mypy_validation.py
@@ -0,0 +1,31 @@
+"""This module provides class Person"""
+
+class Person:
+    """Class for person"""
+    def __init__(self, name: str|None=None, age: int|None=None) -> None:
+        """Method that sets default values or sets velues provided during object creation
+        :params name: Name of the person
+        :params age: Age of the person
+        """
+        self.name = name
+        self.age = age
+
+    def change_age(self, age: int) -> None:
+        """Method to change the age of the person
+        
+        :params age: Age to set
+        """
+        self.age = age
+
+    def change_name(self, name: str) -> None:
+        """Method to change the name of the person
+
+        :params name: Name to set
+        """
+        self.name = name
+
+dave = Person("Dave", 15)
+angel = Person()
+
+print(dave.name)
+print(angel.name)
